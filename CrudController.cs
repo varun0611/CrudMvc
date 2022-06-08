5c2bdba687308a73f73e1ef8a38323793e6b8975
varun0611 <47018192+varun0611@users.noreply.github.com>
2022-06-08 11:09:31 +0530

    Add files via upload

---
+++ CrudController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCCRUD.Models;
+
+namespace MVCCRUD.Controllers
+{
+    public class CrudController : Controller
+    {
+        MvcCrudDBEntities1 ob = new MvcCrudDBEntities1();
+        // GET: Crud
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Insert()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Insert(Customer c)
+        {
+            if (ModelState.IsValid)
+            {
+                ob.Customers.Add(c);
+                ob.SaveChanges();
+                ViewBag.a = "Records inserted Successfully";
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+        public ActionResult DisplayCustomers()
+        {
+            //var res = from t in ob.Customers
+            //          select t;
+
+            var re = ob.Customers.ToList();
+
+            return View(re);
+        }
+    }
+}
